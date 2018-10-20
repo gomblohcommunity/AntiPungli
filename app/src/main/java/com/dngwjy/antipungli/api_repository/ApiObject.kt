11715ebccf63346c7f00ea370e3a40c5a0cab62d
danang <danangwijaya750@gmail.com>
2018-10-20 19:16:28 +0700

    add documentation for ApiReuqest and ApiObject

--- app/src/main/java/com/dngwjy/antipungli/api_repository/ApiObject.kt
+++ app/src/main/java/com/dngwjy/antipungli/api_repository/ApiObject.kt
@@ -2,10 +2,12 @@ package com.dngwjy.antipungli.api_repository
 
 object ApiObject {
 
+	//. url object for user Login
 	fun LoginUrl(username:String,password:String):String{
 		return "https://"
 	}
 
+	//. url object for get nearest parking area
 	fun NearestUrl(latitude:String,longitude:String):String{
 		return "https://"
 	}
--- app/src/main/java/com/dngwjy/antipungli/api_repository/ApiRequest.kt
+++ app/src/main/java/com/dngwjy/antipungli/api_repository/ApiRequest.kt
@@ -3,6 +3,7 @@ package com.dngwjy.antipungli.api_repository
 import java.net.URL
 
 class ApiRequest {
+	//. method for sending http request
 	fun SendingRequest(url:String):String{
 		return URL(url).readText()
 	}
