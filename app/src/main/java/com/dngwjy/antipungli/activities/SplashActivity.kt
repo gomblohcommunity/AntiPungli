e528de6ebe5ebe9becae23905de4b6fbf15f9cc4
Dwi Setiawan <dwisetiawan@linuxmail.org>
2018-10-21 17:01:13 +0700

    Add Splash Screen

---
+++ app/src/main/java/com/dngwjy/antipungli/activities/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.dngwjy.antipungli.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.dngwjy.antipungli.R
+
+class SplashActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_BENGONG : Long = 3000 //3 detik
+    
+    internal val mRunnable: Runnable = Runnable { 
+        if (!isFinishing){
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        
+        mDelayHandler = Handler()
+        
+        mDelayHandler!!.postDelayed(mRunnable,SPLASH_BENGONG)
+    }
+
+    public override fun onDestroy() {
+        if(mDelayHandler != null){
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
+    }
+}
