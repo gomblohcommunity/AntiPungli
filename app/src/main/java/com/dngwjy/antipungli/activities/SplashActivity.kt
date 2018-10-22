8a3dca339072cd279cdf9c28a8f44b9874389d6d
Dwi Setiawan <dwisetiawan@linuxmail.org>
2018-10-22 14:21:28 +0700

    Remove Actionbar adn Force Fullscreen Activity

--- app/src/main/java/com/dngwjy/antipungli/activities/SplashActivity.kt
+++ app/src/main/java/com/dngwjy/antipungli/activities/SplashActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 import com.dngwjy.antipungli.R
 
 class SplashActivity : AppCompatActivity() {
     private var mDelayHandler: Handler? = null
     private val SPLASH_BENGONG : Long = 3000 //3 detik
-    
+
     internal val mRunnable: Runnable = Runnable { 
         if (!isFinishing){
             val intent = Intent(applicationContext, MainActivity::class.java)
@@ -19,6 +21,13 @@ class SplashActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?){
+
+        //.set fullscrenn
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //.hide actionbar
+        supportActionBar!!.hide()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         
